f21efccaa7b8723b37eac7b9fe628dad5b4e6970
mr-learn3r <shubham4ece@gmail.com>
2024-04-20 23:22:29 -0700

    Blinky LED using GPIO driver developed from scratch

---
+++ 0x02_GPIO_Driver_STM32f0x2/driver/Inc/GPIO_drv.h
@@ -0,0 +1,118 @@
+/*
+ * GPIO_drv.h
+ *
+ *  Created on: Apr 15, 2024
+ *  Author: mr_learner
+ *  Desc : This file contains info about function declaration and structs used in GPIO driver
+ */
+
+#ifndef INC_GPIO_DRV_H_
+#define INC_GPIO_DRV_H_
+
+#include "STM32f072b.h"
+
+typedef struct{
+	uint8_t PinNum;                 /*Values from @GPIO_PIN_NUMBERS*/
+	uint8_t PinMode;				/*Values from @GPIO_PIN_MODES*/
+	uint8_t PinSpeed;				/*Values from @GPIO_PIN_SPEED*/
+	uint8_t PinPUPDControl;			/*Values from @GPIO_PIN_PULL_UP_PULL_DOWN*/
+	uint8_t PinOPType;				/*Values from @GPIO_PIN_OUTPUT_TYPE*/
+	uint8_t PinAltFuncMode;			/*Values only when @GPIO_PIN_MODES set to GPIO_MODE_ALTFN (2)*/
+}GPIO_Pinconfig_t;
+
+
+/* GPIO handle structure*/
+
+typedef struct{
+	GPIO_Reg_t *pGPIOx;                 /*	Stores GPIO port base address	*/
+	GPIO_Pinconfig_t *pGPIO_Pinconfig;  /*	Stores GPIO pin configuration setting	*/
+}GPIO_Handle_t;
+
+/*Enum for storing the states of GPIO functions for error checking*/
+typedef enum {
+    GPIO_OK = 0,
+    GPIO_ERROR_INVALID_STATE,
+    GPIO_ERROR_INVALID_GPIO,
+    GPIO_ERROR_INVALID_STATE_VALUE
+} GPIO_Status_t;
+
+/*
+ * @GPIO_PIN_NUMBERS
+ * GPIO pin numbers
+ */
+#define GPIO_PIN_NO_0       0
+#define GPIO_PIN_NO_1       1
+#define GPIO_PIN_NO_2       2
+#define GPIO_PIN_NO_3       3
+#define GPIO_PIN_NO_4       4
+#define GPIO_PIN_NO_5       5
+#define GPIO_PIN_NO_6       6
+#define GPIO_PIN_NO_7       7
+#define GPIO_PIN_NO_8       8
+#define GPIO_PIN_NO_9       9
+#define GPIO_PIN_NO_10      10
+#define GPIO_PIN_NO_11      11
+#define GPIO_PIN_NO_12      12
+#define GPIO_PIN_NO_13      13
+#define GPIO_PIN_NO_14      14
+#define GPIO_PIN_NO_15      15
+
+
+/*
+ * @GPIO_PIN_MODES
+ * GPIO pin possible modes
+ */
+#define GPIO_MODE_IN        0 /* GPIO Input mode                          */
+#define GPIO_MODE_OUT       1 /* GPIO Output mode                         */
+#define GPIO_MODE_ALTFN     2 /* GPIO Alternate functionality             */
+#define GPIO_MODE_ANALOG    3 /* GPIO Analog mode                         */
+#define GPIO_MODE_IT_FT     4 /* GPIO Input mode falling edge trigger     */
+#define GPIO_MODE_IT_RT     5 /* GPIO Input mode rising edge trigger      */
+#define GPIO_MODE_IT_FRT    6 /* GPIO Input mode fall-rising edge trigger */
+
+
+/*
+ * @GPIO_PIN_OUTPUT_TYPE
+ * GPIO pin possible output types
+ */
+#define GPIO_OP_TYPE_PP     0 /* GPIO Output type push-pull mode  */
+#define GPIO_OP_TYPE_OD     1 /* GPIO Output type open-drain mode */
+
+
+/*
+ * @GPIO_PIN_SPEED
+ * GPIO pin possible output speeds
+ */
+#define GPIO_SPEED_LOW      0
+#define GPIO_SPEED_MEDIUM   1
+#define GPIO_SPEED_FAST     2
+#define GPIO_SPEED_HIGH     3
+
+
+/*
+ * @GPIO_PIN_PULL_UP_PULL_DOWN
+ * GPIO pin pull-up and pull-down configuration macros
+ */
+#define GPIO_PIN_NO_PUPD    0 /* GPIO configuration no pull-up, pull-down */
+#define GPIO_PIN_PU         1 /* GPIO configuration pull-up               */
+#define GPIO_PIN_PD         2 /* GPIO configuration pull-down             */
+
+/*=======================================================================================*/
+
+/*								Function declaration of Driver APIs										*/
+
+/* Initialize and Deinitialize */
+GPIO_Status_t GPIO_init							(GPIO_Handle_t *pGPIOhandle);
+void GPIO_deinit						(GPIO_Reg_t *pGPIOx);
+
+/* Peripheral clock*/
+GPIO_Status_t GPIO_pClckCtrl						(GPIO_Reg_t *pGPIOx, uint8_t state);
+
+/*Read and write data operations*/
+uint8_t GPIO_ReadFromInputPin 			(GPIO_Reg_t *pGPIOx, uint8_t PinNum);
+uint16_t GPIO_ReadFromInputPort			(GPIO_Reg_t *pGPIOx);
+uint8_t GPIO_WriteToOutputPin 			(GPIO_Reg_t *pGPIOx, uint8_t PinNum, uint8_t data);
+uint8_t GPIO_WriteToOutputPort			(GPIO_Reg_t *pGPIOx, uint16_t data);
+void GPIO_ToggleOutputPin  				(GPIO_Reg_t *pGPIOx, uint8_t PinNum);
+
+#endif /* INC_GPIO_DRV_H_ */
---
+++ 0x02_GPIO_Driver_STM32f0x2/driver/Src/GPIO_drv.c
@@ -0,0 +1,273 @@
+/*
+ * GPIO_drv.c
+ *
+ *  Created on: Apr 15, 2024
+ *  Author: mr_learner
+ *  Desc : This file contains info about function definition of GPIO_driver
+ */
+
+#include <GPIO_drv.h>
+#include <stdio.h>
+
+
+/* ===============================================================================================
+ * Func name 			: GPIO_init
+ *
+ * Description			: This function initializes GPIO peripherals for a particular port
+ *
+ * Input parameter[0]	: Holds pointer to GPIO handle structure (contains base addr of port & pin config control)
+ *
+ * Return value			: NULL
+ *
+ * ===============================================================================================
+ */
+
+/* Initialize and Deinitialize */
+GPIO_Status_t GPIO_init(GPIO_Handle_t *pGPIOhandle)
+{
+
+    uint32_t pinOffset = (2 * pGPIOhandle->pGPIO_Pinconfig->PinNum);
+
+	/* GPIO pin mode configuration */
+	if(pGPIOhandle->pGPIO_Pinconfig->PinMode<= GPIO_MODE_ANALOG)
+	{
+		/* Non interrupt mode */
+		pGPIOhandle->pGPIOx->MODER  &= ~(0x3 << pinOffset); //Clearing pin
+		pGPIOhandle->pGPIOx->MODER |= (pGPIOhandle->pGPIO_Pinconfig->PinMode << pinOffset);
+	}
+
+    // GPIO pin mode configuration
+    pGPIOhandle->pGPIOx->MODER &= ~(0x3 << pinOffset); // Clearing pin
+    pGPIOhandle->pGPIOx->MODER |= (pGPIOhandle->pGPIO_Pinconfig->PinMode << pinOffset); // Setting pin
+
+    // GPIO pin speed configuration
+    pGPIOhandle->pGPIOx->OSPEEDR &= ~(0x3 << pinOffset); // Clearing pin
+    pGPIOhandle->pGPIOx->OSPEEDR |= (pGPIOhandle->pGPIO_Pinconfig->PinSpeed << pinOffset); // Setting pin
+
+    // GPIO pull up/down settings configuration
+    pGPIOhandle->pGPIOx->PUPDR &= ~(0x3 << pinOffset); // Clearing pin
+    pGPIOhandle->pGPIOx->PUPDR |= (pGPIOhandle->pGPIO_Pinconfig->PinPUPDControl << pinOffset); // Setting pin
+
+	 if(pGPIOhandle->pGPIO_Pinconfig->PinMode == GPIO_MODE_ALTFN)
+	    {
+	        /* Alt functionality registers configuration */
+	        uint8_t LowHigherRegGPIO;
+
+	        LowHigherRegGPIO = pGPIOhandle->pGPIO_Pinconfig->PinNum / 8;
+	        if(LowHigherRegGPIO>7)
+			{
+	        	pGPIOhandle->pGPIOx->AFRL &= ~(0xF << (4 * LowHigherRegGPIO));
+	        	pGPIOhandle->pGPIOx->AFRL |= pGPIOhandle->pGPIO_Pinconfig->PinAltFuncMode << (4 * LowHigherRegGPIO);
+			}
+	        else
+	        {
+	        	pGPIOhandle->pGPIOx->AFRH &= ~(0xF << (4 * LowHigherRegGPIO));
+	        	pGPIOhandle->pGPIOx->AFRH |= pGPIOhandle->pGPIO_Pinconfig->PinAltFuncMode << (4 * LowHigherRegGPIO);
+	        }
+	    }
+	return GPIO_OK;
+}
+
+/* ===============================================================================================
+ * Func name 			: GPIO_deinit
+ *
+ * Description			: This function de-initializes GPIO peripherals for a particular port
+ *
+ * Input parameter[0]	: Holds pointer to GPIO port base addr
+ *
+ * Return value			: NULL
+ *
+ * ===============================================================================================
+ */
+void GPIO_deinit(GPIO_Reg_t *pGPIOx)
+{
+		if(pGPIOx == GPIOA)
+	    {
+	        GPIOA_REG_RESET();
+	    }
+	    else if(pGPIOx == GPIOB)
+	    {
+	        GPIOB_REG_RESET();
+	    }
+	    else if(pGPIOx == GPIOC)
+	    {
+	        GPIOC_REG_RESET();
+	    }
+	    else if(pGPIOx == GPIOD)
+	    {
+	        GPIOD_REG_RESET();
+	    }
+	    else if(pGPIOx == GPIOE)
+	    {
+	        GPIOE_REG_RESET();
+	    }
+	    else if(pGPIOx == GPIOF)
+	    {
+	        GPIOF_REG_RESET();
+	    }
+
+}
+
+/* Peripheral clock*/
+
+/* ===============================================================================================
+ * Func name 			: GPIO_pClckCtrl
+ *
+ * Description			: This function enable or disable peripheral clock  for a particular GPIO port
+ *
+ * Input parameter[0]	: Holds pointer to GPIO port base addr
+ * Input parameter[1]	: Either ENABLE or DISABLE macro (0/1)
+ *
+ * Return value			: NULL
+ *
+ * ===============================================================================================
+ */
+
+GPIO_Status_t GPIO_pClckCtrl(GPIO_Reg_t *pGPIOx, uint8_t state) {
+    if (state != ENABLE && state != DISABLE)
+        return GPIO_ERROR_INVALID_STATE_VALUE;
+
+    switch ((uint32_t)pGPIOx) {
+        case (uint32_t)GPIOA:
+            if (state == ENABLE) GPIOA_PCLCK_ENABLE();
+            else GPIOA_PCLCK_DISABLE();
+            break;
+        case (uint32_t)GPIOB:
+            if (state == ENABLE) GPIOB_PCLCK_ENABLE();
+            else GPIOB_PCLCK_DISABLE();
+            break;
+        case (uint32_t)GPIOC:
+            if (state == ENABLE) GPIOC_PCLCK_ENABLE();
+            else GPIOC_PCLCK_DISABLE();
+            break;
+        case (uint32_t)GPIOD:
+            if (state == ENABLE) GPIOD_PCLCK_ENABLE();
+            else GPIOD_PCLCK_DISABLE();
+            break;
+        case (uint32_t)GPIOE:
+            if (state == ENABLE) GPIOE_PCLCK_ENABLE();
+            else GPIOE_PCLCK_DISABLE();
+            break;
+        case (uint32_t)GPIOF:
+            if (state == ENABLE) GPIOF_PCLCK_ENABLE();
+            else GPIOF_PCLCK_DISABLE();
+            break;
+        default:
+            return GPIO_ERROR_INVALID_GPIO;
+    }
+
+    return GPIO_OK;
+}
+
+
+/*Read and write data operations*/
+/* ===============================================================================================
+ * Func name 			: GPIO_ReadFromInputPin
+ *
+ * Description			: This function reads from an input pin of a particular GPIO port
+ *
+ * Input parameter[0]	: Holds pointer to GPIO port base addr
+ * Input parameter[1]	: Pin Number
+ *
+ * Return value			: Value ranging 0-255
+ *
+ * ===============================================================================================
+ */
+uint8_t GPIO_ReadFromInputPin(GPIO_Reg_t *pGPIOx, uint8_t PinNum)
+{
+	uint8_t value;
+
+	value = (uint8_t )((pGPIOx->IDR >> PinNum) & 0x00000001 );
+
+	return value;
+
+}
+
+/* ===============================================================================================
+ * Func name 			: GPIO_ReadFromInputPort
+ *
+ * Description			: This function reads from an input port of a particular GPIO port
+ *
+ * Input parameter[0]	: Holds pointer to GPIO port base addr
+ *
+ * Return value			: Value ranging [0,2^16-1]
+ *
+ * ===============================================================================================
+ */
+uint16_t GPIO_ReadFromInputPort			(GPIO_Reg_t *pGPIOx)
+{
+	uint16_t value;
+
+	value = (uint16_t )pGPIOx->IDR;
+
+	return value;
+}
+
+/* ===============================================================================================
+ * Func name 			: GPIO_WriteToOutputPin
+ *
+ * Description			: This function writes to an output pin of a particular GPIO port
+ *
+ * Input parameter[0]	: Holds pointer to GPIO port base addr
+ * Input parameter[1]	: Pin Number
+ * Input parameter[2]	: Data ranging 0-255
+ *
+ * Return value			: Value ranging 0-255
+ *
+ * ===============================================================================================
+ */
+uint8_t GPIO_WriteToOutputPin (GPIO_Reg_t *pGPIOx, uint8_t PinNum, uint8_t data)
+{
+	  if(data == SET)
+	    {
+	        /* Write 1 to the output data register at the bit field corresponding to the pin number */
+	        pGPIOx->ODR |= ( 1 << PinNum );
+	    }
+	    else
+	    {
+	        /* Write 0 to the output data register at the bit field corresponding to the pin number */
+	        pGPIOx->ODR &= ~( 1 << PinNum );	//Clear pin
+	    }
+  return data;
+}
+
+/* ===============================================================================================
+ * Func name 			: GPIO_WriteToOutputPort
+ *
+ * Description			: This function writes to an output port of a particular GPIO port
+ *
+ * Input parameter[0]	: Holds pointer to GPIO port base addr
+ * Input parameter[2]	: Data ranging [0,2^16-1]
+ *
+ * Return value			: Value ranging 0-255
+ *
+ * ===============================================================================================
+ */
+uint8_t GPIO_WriteToOutputPort	(GPIO_Reg_t *pGPIOx, uint16_t data)
+{
+	pGPIOx->ODR = data;
+
+	return data?1:0;
+}
+
+/* ===============================================================================================
+ * Func name 			: GPIO_ToggleOutputPin
+ *
+ * Description			: This function toggles an output pin of a particular GPIO port
+ *
+ * Input parameter[0]	: Holds pointer to GPIO port base addr
+ * Input parameter[1]	: Pin Number
+ *
+ * Return value			: NULL
+ *
+ * ===============================================================================================
+ */
+void GPIO_ToggleOutputPin  				(GPIO_Reg_t *pGPIOx, uint8_t PinNum)
+{
+
+	pGPIOx->ODR ^= (1 << PinNum);
+
+}
+
+
+
